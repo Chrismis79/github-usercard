4ce37817ef0cab370366cf329a9169175c479bac
Chrismis79 <chrismis79@gmail.com>
2019-09-07 16:52:36 -0500

    MVP COMPLETE

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,19 @@
            https://api.github.com/users/<your name>
 */
 
+const entryPoint = document.querySelector('.cards');
+
+axios.get('https://api.github.com/users/Chrismis79')
+.then(response => {
+  console.log(response);  
+  const newCard = cardCreator(response.data);
+  entryPoint.append(newCard);  
+  })
+
+.catch(error => {
+  alert("There was an issue retrieving requested data.", error);
+});
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,7 +37,22 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
+axios.get('https://api.github.com/users/Chrismis79/followers')
+.then(response => {
+  console.log(response);  
+  const followersArray = response.data.map((data) => {
+    newCard = cardCreator(data);
+    entryPoint.append(newCard); 
+  });
+   return followersArray;
+  })
+
+.catch(error => {
+  alert("There was an issue retrieving requested data.", error);
+});
+
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +73,52 @@ const followersArray = [];
 </div>
 
 */
+
+
+function cardCreator(data) {
+  //creating elements
+  const 
+  newCard = document.createElement('div'),
+  avatar = document.createElement('img'),
+  cardInfo = document.createElement('div'),
+  name = document.createElement('h3'),
+  username = document.createElement('p'),
+  location = document.createElement('p'),
+  profile = document.createElement('p'),
+  link =document.createElement('a'),
+  followers = document.createElement('p'),
+  following = document.createElement('p'),
+  bio = document.createElement('p');
+
+  //create text content/img src  
+  avatar.src = data.avatar_url;
+  name.textContent = data.name;
+  username.textContent = data.login;
+  location.textContent = data.location;
+  profile.textContent = 'Profile: ';
+  link.href = data.html_url;
+  link.textContent = data.user_html;
+  followers.textContent = `Followers: ${data.followers || "Not Disclosed"}`;
+  following.textContent = `Following: ${data.following || "Not Disclosed"}`;
+  bio.textContent = data.bio;
+
+  //creating classes
+  newCard.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username');
+
+  //setting up element structure
+  entryPoint.append(newCard);
+  newCard.append(avatar);
+  newCard.append(cardInfo);
+  cardInfo.append(name, username, location, profile, followers, following, bio);
+  profile.append(link);
+  
+ 
+  return newCard;
+  
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
